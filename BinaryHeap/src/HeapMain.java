d63c3362f5ca106877af80e06f89dc942f129232
santosc1 <santosc1@csb2103.active.newpaltz.edu>
2015-11-04 10:46:40 -0500

    Working on a solution.

--- BinaryHeap/src/HeapMain.java
+++ BinaryHeap/src/HeapMain.java
@@ -1,40 +1,21 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class HeapMain 
 {
-	static int size = 0;
+
 	public static void main(String [] args)
 	{
 		System.out.println("This is a Binary Heap Program");
-		
-		//Create arrayList to store 20 random integers that will be sorted in the heap
-		
-		int [] heap = new int[20];
-	
-		for(int i = 0; i < 20;i++)
+		PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
+		for(int index = 0; index < 20;index++)
 		{
 			int random =((int)Math.random()* 1000);
-			heap[i] = random;
-			sortUp();
+			heap.add(random);
+			heap.sortUp(index);
 			
 		}
+		for( int index; )
 		
 	}
-	public static void sortUp()
-	{
-		while(hasParent(index)&& )
-	}
 	
-	public static boolean hasParent(int index)
-	{
-		return index > 1;
-	}
-	
-	public static int leftIndex(int index)
-	{
-		return index *2;
-	}
-	
-	
-	
-}
--- BinaryHeap/src/PriorityQueue.java
+++ BinaryHeap/src/PriorityQueue.java
@@ -1,20 +1,73 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class PriorityQueue<E> 
 {
-	
-	public PriorityQueue(){}
-	
-	public void add()
+	static ArrayList<Integer> heap;
+	int heapSize;
+	public PriorityQueue()
 	{
-		Array heap = new Array();
+		heap  =  new ArrayList<Integer>();
+	}
+	
+	public void add(int num)
+	{
+		heap.add(num);
+	}
+	
+	public void sortUp( int index)
+	{
+		int parent = heap.get((index - 1) / 2);
+		int child = heap.get(index);
+		int temp = 0;
+		if(parent < child)
+		{
+			temp = parent;
+			heap.set(parent,child);
+			heap.set(child,temp);
+			sortUp(parent);
+		}
+	
+	}
+	//method that checks the children of the parent and switches with the parent whichever one is greater.
+	public static void sortDown()
+	{
 		
 	}
-	public void siftUp()
+	public
+	public static void print()
+	{
+		if(right != null)
+		{			
+			right.print(level+1);
+		}
+		for(int i = 1; i <= level; i++)
+		{
+			System.out.print("    ");
+			
+		}
+		System.out.println(data);
+		if(left != null)
+		{
+			left.print(level+1);
+		}
+	}
+	
+	public void insert(int number)
+	{
+		i
+		
+		heap.a
+		
+		
+	}
+	
+	public void delete()
 	{
 		
 	}
+	
 	public void siftDown()
 	{
 		
